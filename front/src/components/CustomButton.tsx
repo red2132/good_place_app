aeb528676851c1d3a62a164ce225c90fd3540baa
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-11-27 22:11:10 +0900

    홈화면 수정

--- front/src/components/CustomButton.tsx
+++ front/src/components/CustomButton.tsx
@@ -5,6 +5,7 @@ import {
   Text,
   PressableProps,
   Dimensions,
+  View,
 } from 'react-native';
 import {colors} from '../constants';
 
@@ -35,7 +36,9 @@ export default function CustomButton({
         invalid && styles.invalid,
       ]}
       {...props}>
-      <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+      <View style={styles[size]}>
+        <Text style={[styles.text, styles[`${variant}Text`]]}>{label}</Text>
+      </View>
     </Pressable>
   );
 }
@@ -44,6 +47,7 @@ const styles = StyleSheet.create({
   container: {
     borderRadius: 3,
     justifyContent: 'center',
+    flexDirection: 'row',
   },
   invalid: {
     opacity: 0.5,
@@ -60,12 +64,14 @@ const styles = StyleSheet.create({
     paddingVertical: deviceHeight > 700 ? 15 : 10, // 반응형으로 대응(ios, and 차이)
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
   },
   medium: {
     width: '50%',
     paddingVertical: deviceHeight > 700 ? 12 : 8,
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
   },
   text: {
     fontSize: 16,
--- front/src/screens/auth/AuthHomeScreen.tsx
+++ front/src/screens/auth/AuthHomeScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StackScreenProps} from '@react-navigation/stack';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {Dimensions, Image, SafeAreaView, StyleSheet, View} from 'react-native';
 import {authNavigations} from '../../constants/navigations';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
 import CustomButton from '../../components/CustomButton';
@@ -12,8 +12,15 @@ type AuthHomeScreenProps = StackScreenProps<
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          resizeMode="contain"
+          source={require('../../assets/matzip.png')}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
         <CustomButton
           label="로그인하기"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
@@ -27,6 +34,24 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1.5,
+    width: Dimensions.get('screen').width / 2,
+  },
+  buttonContainer: {
+    flex: 1,
+    gap: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default AuthHomeScreen;
